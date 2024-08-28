03e77358cb376d7f3cd1624e5c035ac4dc7b97da
Vishrut <thevishrutkumar@gmail.com>
2024-08-28 10:16:37 +0530

    yaml branch change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "static",
-  site: 'https://kumarthevishrut.github.io',  // Ensure output is set to static
+  output: "static", 
 });
